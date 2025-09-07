e34340c58a83682f2fb9cf8fbe7d7b3f71c93804
kofi-arthur <paularthurjnr611@gmail.com>
2025-09-07 23:17:12 +0000

    Implements hero section with image slider
    Implements the hero section of the landing page with a vertical image slider.
    
    The slider uses Swiper to display a series of images with an autoplay feature. Includes placeholder content and a call-to-action button.

--- landingPage/src/pages/home.jsx
+++ landingPage/src/pages/home.jsx
@@ -1,7 +1,47 @@
-import React from 'react'
+import 'swiper/css';
+import 'swiper/css/pagination';
+import styles from '../styles/pages/home.module.css'
+
+import { Fragment } from 'react'
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination, Autoplay } from 'swiper/modules';
+
+import Slide1 from '../assets/landing/slide1.webp'
+import Slide2 from '../assets/landing/slide2.webp'
+import Slide3 from '../assets/landing/slide3.webp'
 
 export default function Home() {
     return (
-        <div>Home</div>
+        <Fragment>
+            <section className={styles.heroSection}>
+                <Swiper
+                    direction={'vertical'}
+                    modules={[Autoplay]}
+                    className={styles.swiper}
+
+                    autoplay={{
+                        delay: 3000,
+                        disableOnInteraction: false,
+                    }}
+                    loop={true}
+                >
+                    <SwiperSlide className={styles.swiperSlide}>
+                        <img src={Slide1} />
+                    </SwiperSlide>
+                    <SwiperSlide className={styles.swiperSlide}>
+                        <img src={Slide2} />
+                    </SwiperSlide>
+                    <SwiperSlide className={styles.swiperSlide}>
+                        <img src={Slide3} />
+                    </SwiperSlide>
+                </Swiper>
+                <div className={styles.cover}></div>
+                <div className={styles.content}>
+                    <h1>The Elephant In <br /> Our Community</h1>
+                    <p>Let stories be shared!</p>
+                    <button className={styles.cta}>Join Us Now!</button>
+                </div>
+            </section>
+        </Fragment>
     )
 }
